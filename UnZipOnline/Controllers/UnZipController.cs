0388c0d8be0ec88c661d924dd91e660e4a868591
Illia <kill31072002@gmail.com>
2023-08-21 22:34:24 +0300

    Добавьте файлы проекта.

---
+++ UnZipOnline/Controllers/UnZipController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UnZipOnline.Models;
+
+namespace UnZipOnline.Controllers
+{
+    public class UnZipController : Controller
+    {
+        private readonly ILogger<UnZipController> _logger;
+
+        public UnZipController(ILogger<UnZipController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
